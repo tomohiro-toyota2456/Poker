28e95852ba23e340234e4244db3e5ae00e4c4cf8
harada2 <createdirect2@gmail.com>
2017-05-22 22:12:30 +0900

    Twitter機能の仮入れ

---
+++ Assets/SceneData/Common/Script/Twitter.cs
@@ -0,0 +1,15 @@
+﻿namespace Commmon
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+
+  public class Twitter : MonoBehaviour
+  {
+    public void Tweet(string _text)
+    {
+      string escape = WWW.EscapeURL(_text);
+      Application.OpenURL("https://twitter.com/intent/tweet?text=" + escape);
+    }
+  }
+}
