90bead4ed413580e3bd21dea7168de1f8662dac3
harada2 <createdirect2@gmail.com>
2017-05-27 23:52:18 +0900

    修正

--- Assets/SceneData/Game/Script/GameManager.cs
+++ Assets/SceneData/Game/Script/GameManager.cs
@@ -654,7 +654,7 @@ public class GameManager : MonoBehaviour
     {
       //初期ベットコインに変えてセーブ
       gameUserData.BetCoin = firstBet;
-
+      gameUserData.GetCoinAndSave();
       ContinueCounter = 0;
       gamePopupManager.OpenConfirmPopup(() =>
       {
