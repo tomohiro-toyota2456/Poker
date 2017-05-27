16d71efa5ea0ad96c9e16f85d8d5d8ef02252715
harada2 <createdirect2@gmail.com>
2017-05-27 16:10:20 +0900

    ショップでのシーン遷移までの設定例）キャンバスへのカメラセット　SceneChangerのお作法

---
+++ Assets/SceneData/Shop/Script/ShopManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Common;
+
+public class ShopManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+  {
+    //適当に
+    SceneChanger.Instance.IsInitialize = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
