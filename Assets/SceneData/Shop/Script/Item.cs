bf9314705def22d555b121392f53db836b7104ff
豊田智弘 <toyota@192.168.2.103>
2017-06-04 20:22:50 +0900

    Shop　モック

---
+++ Assets/SceneData/Shop/Script/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+namespace ShopItem
+{
+
+	public class Item : MonoBehaviour {
+
+		public Text itemName { get { return itemName_;} set{ itemName_ = value;}}
+		public int itemValue{ get { return itemValue_; } }
+
+		public void SetUp(ProductData item)
+		{
+			itemName_.text = item.name;
+			itemId_ = item.ItemId;
+			itemValue_ = item.Value;
+
+		}
+
+		[SerializeField]
+		private Text itemName_;
+
+		[SerializeField]
+		private Image icon_;
+
+		[SerializeField]
+		private Text money_;
+
+		private int itemValue_;
+
+		private bool isSoldOut_;
+
+		private string itemId_;
+
+	}
+}
--- Assets/SceneData/Shop/Script/ShopManager.cs
+++ Assets/SceneData/Shop/Script/ShopManager.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Common;
-
+using Common.DataBase;
+using ShopItem;
 public class ShopManager : MonoBehaviour
 {
 
 	// Use this for initialization
 	void Start ()
-  {
-    //適当に
-    SceneChanger.Instance.IsInitialize = true;
+	{
+    	//適当に
+    	SceneChanger.Instance.IsInitialize = true;
+		ShopData = DataBaseManager.Instance.GetDataBase<MasterShopDB> ();
+
+		itemData_ = ShopData.GetDataArray ();
+		foreach (var item in itemData_) 
+		{
+			GameObject itemObj = (GameObject)Instantiate(cell);
+			itemObj.transform.SetParent (content,false);
+			itemObj.GetComponent<Item> ().SetUp (item);
+			itemList.Add (itemObj);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	[SerializeField]
+	private GameObject cell;
+	[SerializeField]
+	private RectTransform content;
+
+	private List<GameObject> itemList = new List<GameObject> ();
+	private MasterShopDB ShopData;
+	private ProductData[] itemData_;
+
 }
